379eb541b1389008d21f0ee3d284e96fdd71bbc4
syauqi sabili <syauqisabili@gmail.com>
2025-01-14 20:03:59 +0700

    added maxrange (km) and changed name attr from range to startrange

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-
+	// Example
 	sentence := "f0016be7c80000020000b87335f239800000000003e219358004014800080052789c95d1cb4ec2401406e0c770a569b925269d96a24151316031a64a8ba0803131be890b763e830bcbc2a41b4d7c43e742efff69ebb79ccb7fe69cd95b0b2fd8d32bb620ceaf624bc1cf994e3de5195be52ccbf8de6c678ecd1eb045643e5f638f9c97b8936e9589b4c06e7646dc15e7baae87c581e3f178820d94a1c4c31cc7214e5e63eaa643198eb04b6c981824ce31dbb2acb34861f7028bb64f133d69809dc4fafd5e869dd2952b47954cc6580f38c6ec6a9664468853bc2c330cc3a2521833812e661835de153bb4b0622ebaedfbf7d226f8fafe89fc6261186eb741b0ade5f3837b97366fed8c0ed62ed3cad2b9268148a0d6f735a12150898256a02ba5cfae2929ddc29ac4ccaa87970f2af654a361d127bf5c559bfe9403252e48b549fe1e3d6be2517a4a03d3b46cf57f8e5675960fadfaa84ea77e45624ae95f142575fd0faa8aca98374432"
 
 	// Convert hex to byte using encoding/hex
@@ -32,10 +32,13 @@ func main() {
 	// Radar
 	radar := &dto.Radar{}
 
+	// Max range (Km)
+	radar.MaxRange = 20
+
 	// Get record header
 	radar.Header = uint32(packet.VideoRecordHeader[0])<<24 | uint32(packet.VideoRecordHeader[1])<<16 | uint32(packet.VideoRecordHeader[2])<<8 | uint32(packet.VideoRecordHeader[3])
 
-	// Compression indicator checking
+	// Get compression indicator
 	radar.DataCompressionIndicator = packet.DataCompressionIndicator>>(8-1) == 1
 
 	// Block data
@@ -63,7 +66,7 @@ func main() {
 		value = int(packet.VideoHeaderNano.EndAzimuth[0])<<8 | int(packet.VideoHeaderNano.EndAzimuth[1])
 		radar.EndAzimuth = (360.0 / math.MaxUint16) * float32(value)
 		// Range
-		radar.Range = uint32(packet.VideoHeaderNano.Range[0])<<24 | uint32(packet.VideoHeaderNano.Range[1])<<16 | uint32(packet.VideoHeaderNano.Range[2])<<8 | uint32(packet.VideoHeaderNano.Range[3])
+		radar.StartRange = uint32(packet.VideoHeaderNano.Range[0])<<24 | uint32(packet.VideoHeaderNano.Range[1])<<16 | uint32(packet.VideoHeaderNano.Range[2])<<8 | uint32(packet.VideoHeaderNano.Range[3])
 		// Cell duration
 		duration := uint32(packet.VideoHeaderNano.Duration[0])<<24 | uint32(packet.VideoHeaderNano.Duration[1])<<16 | uint32(packet.VideoHeaderNano.Duration[2])<<8 | uint32(packet.VideoHeaderNano.Duration[3])
 		radar.CellDuration = float64(duration) * 0.000000000000001
@@ -76,7 +79,7 @@ func main() {
 		value = int(packet.VideoHeaderFemto.EndAzimuth[0])<<8 | int(packet.VideoHeaderFemto.EndAzimuth[1])
 		radar.EndAzimuth = 360.0 / math.MaxUint16 * float32(value)
 		// Range
-		radar.Range = uint32(packet.VideoHeaderFemto.Range[0])<<24 | uint32(packet.VideoHeaderFemto.Range[1])<<16 | uint32(packet.VideoHeaderFemto.Range[2])<<8 | uint32(packet.VideoHeaderFemto.Range[3])
+		radar.StartRange = uint32(packet.VideoHeaderFemto.Range[0])<<24 | uint32(packet.VideoHeaderFemto.Range[1])<<16 | uint32(packet.VideoHeaderFemto.Range[2])<<8 | uint32(packet.VideoHeaderFemto.Range[3])
 		// Cell duration
 		duration := uint32(packet.VideoHeaderFemto.Duration[0])<<24 | uint32(packet.VideoHeaderFemto.Duration[1])<<16 | uint32(packet.VideoHeaderFemto.Duration[2])<<8 | uint32(packet.VideoHeaderFemto.Duration[3])
 		radar.CellDuration = float64(duration) * 0.000000000000001
--- dto/radar.go
+++ dto/radar.go
@@ -5,7 +5,8 @@ type Radar struct {
 	Header                   uint32  `json:"header"`
 	StartAzimuth             float32 `json:"start_azimuth"` // in degrees
 	EndAzimuth               float32 `json:"end_azimuth"`   // in degrees
-	Range                    uint32  `json:"range"`
+	MaxRange                 uint8   `json:"max_range"`
+	StartRange               uint32  `json:"start_range"`
 	CellDuration             float64 `json:"cell_duration"`
 	ValidOctet               uint16  `json:"valid_octet"`
 	ValidCell                uint32  `json:"valid_cell"`
